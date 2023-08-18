e32044a7624d052d76e7f992a009526d15a1d8a2
PaulChen <csz890914@gmail.com>
2023-08-18 21:05:51 +0800

    v0818

--- api/model.py
+++ api/model.py
@@ -9,6 +9,8 @@ from sklearn.ensemble import RandomForestClassifier,VotingClassifier
 from lightgbm import LGBMClassifier
 from catboost import CatBoostClassifier
 from sklearn.preprocessing import LabelEncoder
+from sklearn import metrics
+import time
 
 
 def data_preprocess(file):
@@ -20,6 +22,46 @@ def data_preprocess(file):
     df_drop_unnecessary = df.drop(["id", "Index","sending date","check date","birth date","aid"], axis=1)
     return df_drop_unnecessary
 
+#RFE RF
+def RFE_RF_acc(delete_list,df_drop_unnecessary):
+  df_AccTest = df_drop_unnecessary.drop(labels=['groupno'],axis=1)
+  RFE_acc = []
+  RFE_prec_macro = []
+  RFE_prec_micro = []
+  RFE_prec_weight = []
+  
+  for i in range(len(delete_list)):
+    #取得data & label
+    data = df_AccTest.values # 移除Species並取得剩下欄位資料
+    label = df_drop_unnecessary['groupno'].values
+    le = LabelEncoder()
+    label = le.fit_transform(label)
+    #資料標準化
+    scaler = StandardScaler().fit(data)
+    X_scaledS = scaler.transform(data)
+    #切分資料集與訓練集
+    X_train, X_test, y_train, y_test = train_test_split(X_scaledS, label, test_size=0.1, random_state=42, stratify=label) #資料打散分群
+    # 建立 RandomForestClassifier 模型
+    randomForestModel = RandomForestClassifier(n_estimators=100, criterion = 'gini')
+    randomForestModel.fit(X_train, y_train)
+
+    #print("特徵數: ",len(df_AccTest.columns))
+    #print('訓練集: ',randomForestModel.score(X_train,y_train))
+    #print('測試集: ',randomForestModel.score(X_test,y_test))
+    #存入準確率
+
+    RFE_acc.append(round(randomForestModel.score(X_test,y_test),4))
+    RFE_prec_macro.append(round(metrics.precision_score(y_test, randomForestModel.predict(X_test), average='macro'),4))
+    RFE_prec_micro.append(round(metrics.precision_score(y_test, randomForestModel.predict(X_test), average='micro'),4))
+    RFE_prec_weight.append(round(metrics.precision_score(y_test, randomForestModel.predict(X_test), average='weighted'),4))
+    #去除最小影響特徵
+    df_AccTest = df_AccTest.drop([delete_list[i][0]], axis=1)
+  RFE_acc.reverse()
+  RFE_prec_macro.reverse()
+  RFE_prec_micro.reverse()
+  RFE_prec_weight.reverse()
+
+  return RFE_acc,RFE_prec_macro,RFE_prec_micro,RFE_prec_weight
 
 # RFE Vot
 def RFE_Vot_acc(delete_list,df_drop_unnecessary):
@@ -95,10 +137,16 @@ def RFE_acc_print(RFE_acc,delete_list_CAT):
 
 
 def result(file):
+    start = time.process_time()
     df_drop_unnecessary = data_preprocess(file)
-    delete_list_RF = get_delete_list_RF(df_drop_unnecessary)
-    RFE_acc = RFE_Vot_acc(delete_list_RF,df_drop_unnecessary)
-    return RFE_acc_print(RFE_acc,delete_list_RF)
+    delete_list_CAT = get_delete_list_cat(df_drop_unnecessary)
+    RFE_acc,RFE_prec_macro,RFE_prec_micro,RFE_prec_weight = RFE_RF_acc(delete_list_CAT,df_drop_unnecessary)
+    end = time.process_time()
+    print(end-start)
+    print(RFE_prec_macro[RFE_acc.index(max(RFE_acc))])
+    print(RFE_prec_micro[RFE_acc.index(max(RFE_acc))])
+    print(RFE_prec_weight[RFE_acc.index(max(RFE_acc))])
+    return RFE_acc_print(RFE_acc,delete_list_CAT)
 
 #if __name__ == "__main__":
  #   result('./app/data/cleanresultV4_20210602.xlsx')
